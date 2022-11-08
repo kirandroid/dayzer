3b5b0e5af4e96cd0826b29b9da2bffea9b691495
Kiran Pradhan <pradhanangakiran@gmail.com>
2022-11-08 23:35:35 +0900

    Initial Commit

---
+++ dayzer/dayzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dayzerApp.swift
+//  dayzer
+//
+//  Created by USER on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct dayzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
